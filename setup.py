30bdb41c5d12af0dec4b16d78c29076fdb7d5133
SatyaSrinivas12 <154269094+SatyaSrinivas12@users.noreply.github.com>
2024-08-24 21:47:51 -0400

    Update setup.py
    added ,

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages,setup
 setup(name="multilingual_ai_assistant",
       version='0.1',
       author='Satya Srinivas',
-      packages=find_packages()
+      packages=find_packages(),
 
       python_requires=3.10
       )
